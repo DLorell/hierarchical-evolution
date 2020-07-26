49dea894d048443c6a4a924ed34f12df0dd675cd
DLorell <d.lorell@yahoo.com>
2020-07-26 10:31:14 -0500

    board drawn to screen with grid pattern

--- hill-climber/src/view.py
+++ hill-climber/src/view.py
@@ -4,20 +4,40 @@ from pygame.locals import *
 
 def main():
 
-    board_data = numpy
+    resolution = 1000
+
+    board_data = np.array(
+        [   
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 1, 0],
+            [0, 0, 0, 0, 1, 2, 1, 3, 3, 2, 2, 1],
+            [0, 0, 0, 0, 1, 2, 3, 4, 4, 3, 2, 1],
+            [0, 0, 0, 0, 1, 2, 3, 4, 4, 3, 2, 1],
+            [0, 0, 0, 0, 1, 2, 2, 3, 3, 2, 2, 1],
+            [0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 1, 0],
+            [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0]
+        ]
+    )
+
+    resolution -= resolution % board_data.shape[0]
 
 
-    screen = pygame.display.set_mode((1000,1000))
+    screen = pygame.display.set_mode((resolution,resolution))
     pygame.display.set_caption("TestBoard")
 
 
-    cell_size = 
-    board = pygame.Surface((cell_size*8, cell_size*8))
+    cell_size = resolution // board_data.shape[0]
+    board = pygame.Surface((resolution, resolution))
 
-    board.fill((255, 255, 255))
-    for x in range(0, 8, 2):
-        for y in range(0, 8, 2):
-            pygame.draw.rect(board, (0,0,0), (x*cell_size, y*cell_size, cell_size, cell_size))
+    board.fill((0, 0, 0))
+    for x in range(0, board_data.shape[0]):
+        for y in range(0, board_data.shape[1]):
+            pygame.draw.rect(board, (212,175,55), (x*cell_size, y*cell_size, cell_size-1, cell_size-1))
+            
 
 
     # Event loop
