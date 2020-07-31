0c5271de17a3bb691872cf6893aee948c8997004
DLorell <d.lorell@yahoo.com>
2020-07-31 00:21:40 -0500

    pushing to remote

---
+++ hill-climber/src/controller.py
@@ -0,0 +1,46 @@
+import pygame
+import time
+import numpy as np
+from pygame.locals import QUIT
+from src.view import BoardView
+from src.model import Agent, Environment
+
+class EvoSim():
+    def __init__(self):
+        self.map_size = 250
+        self.max_pop = 100
+        self.k = 0
+
+        self.tick_rate = 0.01
+
+    def start(self):
+        pygame.init()
+
+        environment = Environment(h=self.map_size, w=self.map_size)
+        environment.k = self.k
+        environment.max_pop = self.max_pop
+
+        bview = BoardView(environment.heightmap, [(a.x, a.y) for a in environment.agents])
+
+        t = time.time()
+        while 1:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    return
+                   
+            if time.time() - t >= self.tick_rate:
+                t = time.time()
+                environment.step()
+                bview.set_agents([(a.x, a.y) for a in environment.agents])
+                bview.update_board()
+
+
+
+    def __repr__(self):
+        # TODO
+        return super().__repr__()
+
+    def __str__(self):
+        # TODO
+        return super().__str__()
+
---
+++ hill-climber/tests/test_controller.py
@@ -0,0 +1,8 @@
+import src.view as view
+import src.model as model
+import src.controller as controller
+import pytest
+import pytest_check as check
+import numpy as np
+import os
+os.environ['SDL_VIDEODRIVER']='dummy'
